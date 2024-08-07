1ce286707410eb0aaff371f79b27b95dd7e1684a
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-08-07 12:16:41 +0200

    move kernel from root to kernel/

--- tools/mkiso-amd64.sh
+++ tools/mkiso-amd64.sh
@@ -7,7 +7,7 @@ ISOROOT=build-${PORT}/isoroot
 ISO=build-${PORT}/nyy.iso
 
 mkdir -p "${ISOROOT}" &&
-	cp build-${PORT}/platform/amd64/symbols.map build-${PORT}/platform/amd64/nyy ${LIMINE}/limine-bios.sys ${LIMINE}/limine-bios-cd.bin ${LIMINE}/limine-uefi-cd.bin "${ISOROOT}" &&
+	cp build-${PORT}/kernel/platform/amd64/symbols.map build-${PORT}/kernel/platform/amd64/nyy ${LIMINE}/limine-bios.sys ${LIMINE}/limine-bios-cd.bin ${LIMINE}/limine-uefi-cd.bin "${ISOROOT}" &&
 	cp tools/limine-amd64.cfg "${ISOROOT}/limine.cfg" &&
 	xorriso -as mkisofs -b limine-bios-cd.bin \
 		-no-emul-boot -boot-load-size 4 -boot-info-table \
