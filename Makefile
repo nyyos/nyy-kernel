4635913016083086e4c79e742a268785b4d9cbf5
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-08-25 21:53:16 +0200

    mtools

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 export ARCH ?= amd64
+export LANG=c
 
 all:
 	@meson compile -C build-$(ARCH)
--- tools/mkiso-amd64-hyper.sh
+++ tools/mkiso-amd64-hyper.sh
@@ -2,38 +2,33 @@
 
 export LANG=c
 
+source "$(dirname "$0")/common.sh"
+
 PORT=amd64
 ISOROOT=build-${PORT}/isoroot-hyper
 ISO=build-${PORT}/nyy-${PORT}-hyper.iso
 HYPER_RELEASE=v0.8.0
-HYPER_RELEASE_URL="https://github.com/UltraOS/Hyper/releases/download/${HYPER_RELEASE}"
-HYPER=build-${PORT}/hyper-${HYPER_RELEASE}
-
-function dl_hyper() {
-	if [[ ! -f ${HYPER}/$1 ]]; then
-		echo "downloading $1 ..."
-		curl -sL "${HYPER_RELEASE_URL}/$1" -o "${HYPER}/$1"
-	fi
-}
+HYPER_URL="https://github.com/UltraOS/Hyper/releases/download/${HYPER_RELEASE}"
+HYPER="build-${PORT}/hyper-${HYPER_RELEASE}"
 
 mkdir -p "${HYPER}"
 
-dl_hyper hyper_iso_boot
-dl_hyper BOOTX64.EFI
-dl_hyper hyper_install-linux-x86_64
-chmod +x "${HYPER}/hyper_install-linux-x86_64"
+fetch "${HYPER}/hyper_iso_boot" "${HYPER_URL}/hyper_iso_boot"
+fetch "${HYPER}/BOOTX64.EFI" "${HYPER_URL}/BOOTX64.EFI"
+if [ "$(uname)" = "Linux" ]; then
+	fetch "${HYPER}/hyper_install" "${HYPER_URL}/hyper_install-linux-x86_64"
+	chmod +x "${HYPER}/hyper_install"
+else
+	echo "There is no native hyper_install available for your platform." 1>&2
+	exit 1
+fi
 
 if [[ ! -f ${HYPER}/esp.img ]]; then
-	dd if=/dev/zero of="${HYPER}/esp.img" iflag=fullblock bs=512K count=1
-	mkfs.fat "${HYPER}/esp.img"
-	device="$(udisksctl loop-setup -f "${HYPER}/esp.img" | awk '{ print $5 }'|tr -d '.')"
-	mp="$(udisksctl mount -b "$device" | awk '{print $4}')"
-
-	mkdir -p "${mp}/EFI/BOOT/"
-	cp "${HYPER}/BOOTX64.EFI" "${mp}/EFI/BOOT/"
-
-	udisksctl unmount -b "$device"
-	udisksctl loop-delete -b "${device}"
+	dd if=/dev/zero of="${HYPER}/esp.img" iflag=fullblock bs=1024K count=1
+	mformat -i "${HYPER}/esp.img"
+	mmd -i "${HYPER}/esp.img" ::/EFI
+	mmd -i "${HYPER}/esp.img" ::/EFI/BOOT
+	mcopy -i "${HYPER}/esp.img" "${HYPER}/BOOTX64.EFI" ::/EFI/BOOT/
 fi
 
 mkdir -p "${ISOROOT}" &&
@@ -48,4 +43,4 @@ mkdir -p "${ISOROOT}" &&
 		--efi-boot esp.img -efi-boot-part --efi-boot-image \
 		--protective-msdos-label \
 		"${ISOROOT}" -o "${ISO}" &&
-	"${HYPER}/hyper_install-linux-x86_64" "${ISO}"
+	"${HYPER}/hyper_install" "${ISO}"
