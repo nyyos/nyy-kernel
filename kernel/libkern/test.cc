82d944117faca187d4bfdc6b3e49d62857e0e3b4
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-10-16 17:36:24 +0200

    change test.cc a little bit..

--- kernel/libkern/test.cc
+++ kernel/libkern/test.cc
@@ -1,3 +1,4 @@
+#include "DevKit/IORegistryEntry.h"
 #include "libkern/OSArray.h"
 #include "libkern/OSDictionary.h"
 #include "libkern/OSString.h"
@@ -50,66 +51,89 @@ OSDefineMetaClassAndStructors(Bar, Foo);
 
 void test_fn_cpp()
 {
-	auto inst = Foo::gMetaClass.alloc();
-	inst->init();
-	auto inst2 = Bar::gMetaClass.alloc();
-	inst2->init();
-	auto inst3 = Bar::gMetaClass.alloc();
-	inst3->init();
-
-	auto asFoo = inst3->safe_cast<Foo>();
-	assert(asFoo != nullptr);
-	auto asBar = asFoo->safe_cast<Bar>();
-	assert(asBar != nullptr);
-
-	inst3->release();
-	inst2->release();
-	inst->release();
-
-	char buf[512];
-	memset(buf, 0x0, sizeof(buf));
-	memcpy(buf, "HELLO WORLD", sizeof("HELLO WORLD"));
-	auto str1 = OSString::fromCStr(buf);
-	auto str2 = OSString::fromStr(str1);
-	assert(str1->getCStr() != str2->getCStr());
-	str1->release();
-	str2->release();
-
-	auto dict = (OSDictionary *)OSDictionary::gMetaClass.alloc();
-	dict->init();
 	{
-		auto str = OSString::fromCStr("Hello World");
-		dict->set("test", str);
+		auto inst = OSAlloc<Foo>();
+		inst->init();
+		auto inst2 = OSAlloc<Bar>();
+		inst2->init();
+		auto inst3 = OSAlloc<Bar>();
+		inst3->init();
+
+		auto asFoo = inst3->safe_cast<Foo>();
+		assert(asFoo != nullptr);
+		auto asBar = asFoo->safe_cast<Bar>();
+		assert(asBar != nullptr);
+
+		inst3->release();
+		inst2->release();
+		inst->release();
 	}
 
-	OSString *str;
 	{
-		auto res = dict->get(OSSymbol::fromCStr("test"));
-		assert(res);
-		str = res->safe_cast<OSString>();
+		char buf[512];
+		memset(buf, 0x0, sizeof(buf));
+		memcpy(buf, "HELLO WORLD", sizeof("HELLO WORLD"));
+		auto str1 = OSString::fromCStr(buf);
+		auto str2 = OSString::fromStr(str1);
+		assert(str1->getCStr() != str2->getCStr());
+		assert(str1->getRefcount() == 1);
+		assert(str2->getRefcount() == 1);
+		str1->release();
+		str2->release();
+	}
+
+	{
+		auto dict = OSDictionary::makeEmpty();
+		auto str_orig = OSString::fromCStr("Hello World");
+		{
+			dict->set("test", str_orig);
+			auto sym = OSSymbol::fromCStr("test");
+			assert(sym->getRefcount() == 2);
+			sym->release();
+			str_orig->release();
+			assert(str_orig->getRefcount() == 1);
+		}
+
+		OSString *str;
+		{
+			auto res = dict->get("test");
+			assert(res);
+			str = res->safe_cast<OSString>();
+			assert(str == str_orig);
+			assert(str->getRefcount() == 2);
+		}
+
+		assert(dict->getRefcount() == 1);
+		dict->release();
+		assert(str->getRefcount() == 1);
 		str->release();
 	}
 
-	dict->release();
-
-	auto testmap = HashMap<int, int>();
-	testmap.insert(1, 1);
-
 	{
-		auto sym1 = OSSymbol::fromCStr("test");
-		auto str = OSString::fromCStr("test");
-		auto sym2 = OSSymbol::fromStr(str);
-		str->release();
-		assert(sym1 == sym2);
-		sym1->release(1);
+		auto testmap = HashMap<int, int>();
+		testmap.insert(1, 1);
+
+		{
+			auto sym1 = OSSymbol::fromCStr("test");
+			auto str = OSString::fromCStr("test");
+			auto sym2 = OSSymbol::fromStr(str);
+			assert(str->getRefcount() == 1);
+			str->release();
+			assert(sym1 == sym2);
+			assert(sym1->getRefcount() == 2);
+			sym1->release();
+			sym2->release();
+		}
 	}
 
-	auto mut = lk::Mutex();
-	lk::unique_lock<lk::Mutex> uniq;
 	{
-		const auto guard = lk::lock_guard(mut);
-		uniq = lk::unique_lock(mut, lk::adopt_lock_t());
-		uniq.release();
+		auto mut = lk::Mutex();
+		lk::unique_lock<lk::Mutex> uniq;
+		{
+			const auto guard = lk::lock_guard(mut);
+			uniq = lk::unique_lock(mut, lk::adopt_lock_t());
+			uniq.release();
+		}
 	}
 
 	{
@@ -126,8 +150,14 @@ void test_fn_cpp()
 		assert(arr->pop() == sym);
 		assert(arr->pop() == sym);
 		sym->release();
+		assert(sym->getRefcount() == 8);
+		sym->release();
+		sym->release();
 		assert(arr->getCount() == 6);
+		arr->release();
 	}
+
+	IORegistryEntry::InitRegistry();
 }
 
 extern "C" {
