30e66ab604e3300602eb07a7d1178a3a1c364863
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-11-01 17:42:52 +0100

    test framework

--- kernel/tests/runner.c
+++ kernel/tests/runner.c
@@ -6,12 +6,38 @@ typedef struct test_result (*test_function)();
 
 extern struct test_result mutex_test();
 
-test_function tests[] = { mutex_test };
+struct test_entry {
+	const char *test_name;
+	test_function fn;
+};
+
+static struct test_entry tests[] = { { "mutex acq/rel", mutex_test } };
+static struct test_result test_results[elementsof(tests)];
+static int passed, skipped, errors;
 
 void test_runner()
 {
 	for (int i = 0; i < elementsof(tests); i++) {
 		printk(INFO "Running test %d/%ld\n", i + 1, elementsof(tests));
-		tests[i]();
+		struct test_entry *ent = &tests[i];
+		test_results[i] = ent->fn();
+		switch (test_results[i].status) {
+		case test_OK:
+			passed++;
+			break;
+		case test_SKIP:
+			skipped++;
+			break;
+		case test_ERR:
+			printk(WARN "test '%s' failed: %s\n",
+			       tests[i].test_name, test_results[i].err_msg);
+			errors++;
+			break;
+		}
 	}
+
+	printk(INFO "TEST SUMMARY\n");
+	printk(INFO "Passed: %d\n", passed);
+	printk(INFO "Skipped: %d\n", skipped);
+	printk(INFO "Failed: %d\n", errors);
 }
